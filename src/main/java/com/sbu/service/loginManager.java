d13544741f1bfdf98ad320d4bf597e5cf21d822e
paeco <z.hosseininm@gmail.com>
2017-05-24 09:35:43 +0430

    First Commit

---
+++ src/main/java/com/sbu/service/loginManager.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sbu.service;
+import com.sbu.dao.model.Person;
+/**
+ *
+ * @author WIN 10
+ */
+public class loginManager {
+    
+}
